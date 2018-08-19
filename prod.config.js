d0c3814b1b8fa433bc15e8e8b9b760e67244bf35
Osama Najjar <osama.najjar.m@gmail.com>
2018-08-19 08:11:58 +0200

    reset local 7 remote repo

---
+++ prod.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: "production",
+  entry: ["./src/index.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["babel-preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use:ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use:["css-loader", "sass-loader"]
+        })
+         
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("style.css"),
+    new HtmlWebpackPlugin({
+        inject : false,
+        template: './src/index.html',
+        filename: 'index.html'
+    })
+  ]
+};
