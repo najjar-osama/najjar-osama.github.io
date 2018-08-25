c47b5a1ecd59a3ae73a12ee2a2c6f7cafbd729e4
Osama Najjar <osama.najjar.m@gmail.com>
2018-08-25 08:17:49 +0200

    updated scrolling behavior

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,31 +1,33 @@
-const setScrollHandler = ()=>{
-    const scrollStep = document.body.clientHeight;
-    if(!window.lastScrollStep){
-        window.lastScrollStep = window.scrollY;
+const setScrollHandler = () => {
+  const scrollStep = document.body.clientHeight;
+  if (!window.lastScrollStep) {
+    window.lastScrollStep = window.scrollY;
+  }
+  window.addEventListener("scroll", e => {
+    e.preventDefault(); // does nothing becasue the event is not cancelable
+    const nav = document.getElementById("nav");
+    if (window.scrollY > window.lastScrollStep) {
+      if (!Number.isInteger(window.scrollY / window.lastScrollStep)) {
+        window.scrollTo({
+          left: 0,
+          top: scrollStep,
+          behavior: "auto"
+        });
+        window.lastScrollStep = window.lastScrollStep + scrollStep;
+        nav.classList.add("nav--dark");
+      }
+    } else {
+      if (!Number.isInteger(window.scrollY / window.lastScrollStep)) {
+      }
+      window.scrollTo({
+        left: 0,
+        top: window.lastScrollStep - scrollStep,
+        behavior: "auto"
+      });
+      window.lastScrollStep = window.lastScrollStep - scrollStep;
+      window.scrollY === 0 ? nav.classList.remove("nav--dark") : nav;
     }
-    window.addEventListener("scroll",(e)=>{
-        //e.preventDefault(); // does nothing becasue the event is not cancelable
-        const nav = document.getElementById("nav");
-        if(window.scrollY > window.lastScrollStep){
-            
-            
-            window.scrollTo({
-                left : 0,
-                top:scrollStep,
-                behavior : "smooth"
-            }); 
-            nav.classList.add("nav--dark");
-            
-        }else{
-            window.scrollTo({
-                left : 0,
-                top: window.lastScrollStep - scrollStep,
-                behavior : "smooth"
-            });
-            window.scrollY === 0 ? nav.classList.remove("nav--dark") : nav;
-        }
-        window.lastScrollStep = window.scrollY;
-    });
+  });
 };
 
 export default setScrollHandler;
