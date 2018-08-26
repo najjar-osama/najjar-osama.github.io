6fd8afb62e21d0f110705204309ad18f04dbc92e
Osama Najjar <osama.najjar.m@gmail.com>
2018-08-26 22:01:22 +0200

    edited carousel options

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -3,8 +3,12 @@ import Flickity from "flickity";
 const initCarousel = () => {
   const projectsCarouselWrapper = document.getElementById("projectsCarousel");
   const carousel = new Flickity(projectsCarouselWrapper, {
+    autoPlay: 3000,
     cellAlign: "center",
-    contain: true
+    contain: true,
+    pauseAutoPlayOnHover: true,
+    freeScroll : true,
+    wrapAround : true
   });
 };
 
