9b2f12cf4ddbe3c79bbe072ae5f93f272026d760
Osama Najjar <osama.najjar.m@gmail.com>
2018-09-02 16:09:37 +0200

    updated layout page paading

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -4,19 +4,19 @@ const setScrollHandler = () => {
     window.lastScrollStep = window.scrollY;
   }
   window.addEventListener("scroll", e => {
-    e.preventDefault(); // does nothing becasue the event is not cancelable
+    //e.preventDefault(); // does nothing becasue the event is not cancelable
     const nav = document.getElementById("nav");
     if (window.scrollY > window.lastScrollStep) {
       if (!Number.isInteger(window.scrollY / window.lastScrollStep)) {
         window.lastScrollStep = window.lastScrollStep + scrollStep;
         nav.classList.add("nav--dark");
       }
+      
     } else {
-      if (!Number.isInteger(window.scrollY / window.lastScrollStep)) {
-      }
       window.lastScrollStep = window.lastScrollStep - scrollStep;
       window.scrollY === 0 ? nav.classList.remove("nav--dark") : nav;
-    }
+      
+    } 
   });
 };
 
