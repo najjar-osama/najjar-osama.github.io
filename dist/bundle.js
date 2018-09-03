1c7adca10dafe9c1525067478316d5f5eb60a28a
Osama Najjar <osama.najjar.m@gmail.com>
2018-09-03 02:29:14 +0200

    finialized contact section

--- dist/bundle.js
+++ dist/bundle.js
@@ -372,6 +372,17 @@ eval("module.exports = __webpack_require__.p + \"images/dekra.png\";\n\n//# sour
 
 /***/ }),
 
+/***/ "./src/images/email.png":
+/*!******************************!*\
+  !*** ./src/images/email.png ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__.p + \"images/email.png\";\n\n//# sourceURL=webpack:///./src/images/email.png?");
+
+/***/ }),
+
 /***/ "./src/images/favicon.png":
 /*!********************************!*\
   !*** ./src/images/favicon.png ***!
@@ -424,6 +435,17 @@ eval("module.exports = __webpack_require__.p + \"images/gulp.png\";\n\n//# sourc
 /***/ (function(module, exports, __webpack_require__) {
 
 eval("module.exports = __webpack_require__.p + \"images/html.png\";\n\n//# sourceURL=webpack:///./src/images/html.png?");
+
+/***/ }),
+
+/***/ "./src/images/in.png":
+/*!***************************!*\
+  !*** ./src/images/in.png ***!
+  \***************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__.p + \"images/in.png\";\n\n//# sourceURL=webpack:///./src/images/in.png?");
 
 /***/ }),
 
@@ -526,6 +548,17 @@ eval("module.exports = __webpack_require__.p + \"images/node.png\";\n\n//# sourc
 
 /***/ }),
 
+/***/ "./src/images/pgithub.png":
+/*!********************************!*\
+  !*** ./src/images/pgithub.png ***!
+  \********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__.p + \"images/pgithub.png\";\n\n//# sourceURL=webpack:///./src/images/pgithub.png?");
+
+/***/ }),
+
 /***/ "./src/images/react.png":
 /*!******************************!*\
   !*** ./src/images/react.png ***!
@@ -548,6 +581,17 @@ eval("module.exports = __webpack_require__.p + \"images/sass.png\";\n\n//# sourc
 
 /***/ }),
 
+/***/ "./src/images/twitter.png":
+/*!********************************!*\
+  !*** ./src/images/twitter.png ***!
+  \********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__.p + \"images/twitter.png\";\n\n//# sourceURL=webpack:///./src/images/twitter.png?");
+
+/***/ }),
+
 /***/ "./src/images/webpack.png":
 /*!********************************!*\
   !*** ./src/images/webpack.png ***!
@@ -567,7 +611,7 @@ eval("module.exports = __webpack_require__.p + \"images/webpack.png\";\n\n//# so
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-eval("\n\n__webpack_require__(/*! ./images/intro_bg.png */ \"./src/images/intro_bg.png\");\n\n__webpack_require__(/*! ./images/me_1.jpg */ \"./src/images/me_1.jpg\");\n\n__webpack_require__(/*! ./images/me_5.jpg */ \"./src/images/me_5.jpg\");\n\n__webpack_require__(/*! ./images/favicon.png */ \"./src/images/favicon.png\");\n\n__webpack_require__(/*! ./images/bg_pattern.png */ \"./src/images/bg_pattern.png\");\n\n__webpack_require__(/*! ./images/github.png */ \"./src/images/github.png\");\n\n__webpack_require__(/*! ./images/link.png */ \"./src/images/link.png\");\n\n__webpack_require__(/*! ./images/mx_pwa.png */ \"./src/images/mx_pwa.png\");\n\n__webpack_require__(/*! ./images/mx_camera.png */ \"./src/images/mx_camera.png\");\n\n__webpack_require__(/*! ./images/mx_cke.png */ \"./src/images/mx_cke.png\");\n\n__webpack_require__(/*! ./images/dbw.png */ \"./src/images/dbw.png\");\n\n__webpack_require__(/*! ./images/dekra.png */ \"./src/images/dekra.png\");\n\n__webpack_require__(/*! ./images/js.png */ \"./src/images/js.png\");\n\n__webpack_require__(/*! ./images/html.png */ \"./src/images/html.png\");\n\n__webpack_require__(/*! ./images/css.png */ \"./src/images/css.png\");\n\n__webpack_require__(/*! ./images/git.png */ \"./src/images/git.png\");\n\n__webpack_require__(/*! ./images/webpack.png */ \"./src/images/webpack.png\");\n\n__webpack_require__(/*! ./images/sass.png */ \"./src/images/sass.png\");\n\n__webpack_require__(/*! ./images/react.png */ \"./src/images/react.png\");\n\n__webpack_require__(/*! ./images/node.png */ \"./src/images/node.png\");\n\n__webpack_require__(/*! ./images/gulp.png */ \"./src/images/gulp.png\");\n\n__webpack_require__(/*! ./images/cmd.png */ \"./src/images/cmd.png\");\n\n__webpack_require__(/*! ./styles.scss */ \"./src/styles.scss\");\n\n__webpack_require__(/*! ../node_modules/normalize.scss/normalize.scss */ \"./node_modules/normalize.scss/normalize.scss\");\n\nvar _jquery = __webpack_require__(/*! jquery */ \"./node_modules/jquery/dist/jquery.js\");\n\nvar _jquery2 = _interopRequireDefault(_jquery);\n\n__webpack_require__(/*! ./js/scrollit.js */ \"./src/js/scrollit.js\");\n\nvar _scroll = __webpack_require__(/*! ./js/scroll */ \"./src/js/scroll.js\");\n\nvar _scroll2 = _interopRequireDefault(_scroll);\n\n__webpack_require__(/*! ../node_modules/flickity/css/flickity.css */ \"./node_modules/flickity/css/flickity.css\");\n\nvar _load = __webpack_require__(/*! ./js/load */ \"./src/js/load.js\");\n\nvar _carousel = __webpack_require__(/*! ./js/carousel */ \"./src/js/carousel.js\");\n\nvar _carousel2 = _interopRequireDefault(_carousel);\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\n//import setScrollHandler from \"./js/scroll\";\n\n\n(0, _load.load)();\n(0, _scroll2.default)();\n(0, _carousel2.default)();\n(0, _jquery2.default)(function () {\n    _jquery2.default.scrollIt({\n        upKey: 38, // key code to navigate to the next section\n        downKey: 40, // key code to navigate to the previous section\n        easing: 'linear', // the easing function for animation\n        scrollTime: 600, // how long (in ms) the animation takes\n        activeClass: 'active', // class given to the active nav element\n        onPageChange: null, // function(pageIndex) that is called when page is changed\n        topOffset: 0 // offste (in px) for fixed top navigation\n    });\n});\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("\n\n__webpack_require__(/*! ./images/intro_bg.png */ \"./src/images/intro_bg.png\");\n\n__webpack_require__(/*! ./images/me_1.jpg */ \"./src/images/me_1.jpg\");\n\n__webpack_require__(/*! ./images/me_5.jpg */ \"./src/images/me_5.jpg\");\n\n__webpack_require__(/*! ./images/favicon.png */ \"./src/images/favicon.png\");\n\n__webpack_require__(/*! ./images/bg_pattern.png */ \"./src/images/bg_pattern.png\");\n\n__webpack_require__(/*! ./images/github.png */ \"./src/images/github.png\");\n\n__webpack_require__(/*! ./images/link.png */ \"./src/images/link.png\");\n\n__webpack_require__(/*! ./images/mx_pwa.png */ \"./src/images/mx_pwa.png\");\n\n__webpack_require__(/*! ./images/mx_camera.png */ \"./src/images/mx_camera.png\");\n\n__webpack_require__(/*! ./images/mx_cke.png */ \"./src/images/mx_cke.png\");\n\n__webpack_require__(/*! ./images/dbw.png */ \"./src/images/dbw.png\");\n\n__webpack_require__(/*! ./images/dekra.png */ \"./src/images/dekra.png\");\n\n__webpack_require__(/*! ./images/js.png */ \"./src/images/js.png\");\n\n__webpack_require__(/*! ./images/html.png */ \"./src/images/html.png\");\n\n__webpack_require__(/*! ./images/css.png */ \"./src/images/css.png\");\n\n__webpack_require__(/*! ./images/git.png */ \"./src/images/git.png\");\n\n__webpack_require__(/*! ./images/webpack.png */ \"./src/images/webpack.png\");\n\n__webpack_require__(/*! ./images/sass.png */ \"./src/images/sass.png\");\n\n__webpack_require__(/*! ./images/react.png */ \"./src/images/react.png\");\n\n__webpack_require__(/*! ./images/node.png */ \"./src/images/node.png\");\n\n__webpack_require__(/*! ./images/gulp.png */ \"./src/images/gulp.png\");\n\n__webpack_require__(/*! ./images/cmd.png */ \"./src/images/cmd.png\");\n\n__webpack_require__(/*! ./images/email.png */ \"./src/images/email.png\");\n\n__webpack_require__(/*! ./images/in.png */ \"./src/images/in.png\");\n\n__webpack_require__(/*! ./images/twitter.png */ \"./src/images/twitter.png\");\n\n__webpack_require__(/*! ./images/pgithub.png */ \"./src/images/pgithub.png\");\n\n__webpack_require__(/*! ./styles.scss */ \"./src/styles.scss\");\n\n__webpack_require__(/*! ../node_modules/normalize.scss/normalize.scss */ \"./node_modules/normalize.scss/normalize.scss\");\n\nvar _jquery = __webpack_require__(/*! jquery */ \"./node_modules/jquery/dist/jquery.js\");\n\nvar _jquery2 = _interopRequireDefault(_jquery);\n\n__webpack_require__(/*! ./js/scrollit.js */ \"./src/js/scrollit.js\");\n\nvar _scroll = __webpack_require__(/*! ./js/scroll */ \"./src/js/scroll.js\");\n\nvar _scroll2 = _interopRequireDefault(_scroll);\n\n__webpack_require__(/*! ../node_modules/flickity/css/flickity.css */ \"./node_modules/flickity/css/flickity.css\");\n\nvar _load = __webpack_require__(/*! ./js/load */ \"./src/js/load.js\");\n\nvar _carousel = __webpack_require__(/*! ./js/carousel */ \"./src/js/carousel.js\");\n\nvar _carousel2 = _interopRequireDefault(_carousel);\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\n//import setScrollHandler from \"./js/scroll\";\n\n\n(0, _load.load)();\n(0, _scroll2.default)();\n(0, _carousel2.default)();\n(0, _jquery2.default)(function () {\n    _jquery2.default.scrollIt({\n        upKey: 38, // key code to navigate to the next section\n        downKey: 40, // key code to navigate to the previous section\n        easing: 'linear', // the easing function for animation\n        scrollTime: 600, // how long (in ms) the animation takes\n        activeClass: 'active', // class given to the active nav element\n        onPageChange: null, // function(pageIndex) that is called when page is changed\n        topOffset: 0 // offste (in px) for fixed top navigation\n    });\n});\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ }),
 
